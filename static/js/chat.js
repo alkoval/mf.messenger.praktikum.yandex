26a6774cb6b2bc38f78a747f83b7c3655ecdce71
alkoval <koval.alk@yandex.ru>
2021-01-10 13:10:11 +0500

    update from code review

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,24 +1,20 @@
 function toggleModalDialog() {
-  const md = document.getElementById("mdDialog");
-  if (md.classList.contains("modal_state_hide")) {
-    md.classList.remove("modal_state_hide");
-    md.classList.add("modal_state_show");
-  } else {
+  const md = document.getElementsByClassName('modal modal_position-right-top')[0];
+  if (md.classList.contains("modal_state_show")) {
     md.classList.remove("modal_state_show");
-    md.classList.add("modal_state_hide");
+  } else {
+    md.classList.add("modal_state_show");
   }
 }
 
 function toggleModalClip() {
-  const md = document.getElementById("mdClip");
-  const btn = document.getElementById("btnClip");
+  const md = document.getElementsByClassName('modal modal_position_left-end')[0];
+  const btn = document.getElementsByClassName('history__button history__button_type_text')[0];
   md.style.top = btn.offsetTop;
   md.style.left = btn.offsetLeft;
-  if (md.classList.contains("modal_state_hide")) {
-    md.classList.remove("modal_state_hide");
-    md.classList.add("modal_state_show");
-  } else {
+  if (md.classList.contains("modal_state_show")) {
     md.classList.remove("modal_state_show");
-    md.classList.add("modal_state_hide");
+  } else {
+    md.classList.add("modal_state_show");
   }
 }
